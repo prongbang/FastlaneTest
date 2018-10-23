12ea353468189b67db5303273622e4aea32db33b
prongbang <prongbang@gmail.com>
2018-10-23 18:02:36 +0700

    migrate to androidx

--- app/src/androidTest/java/com/touchtechnologies/fastlanetest/RevertTextBehaviorTest.java
+++ app/src/androidTest/java/com/touchtechnologies/fastlanetest/RevertTextBehaviorTest.java
@@ -1,8 +1,8 @@
 package com.touchtechnologies.fastlanetest;
 
-import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
 import com.touchtechnologies.fastlanetest.utils.StringUtil;
 
@@ -18,12 +18,12 @@ import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
 import tools.fastlane.screengrab.locale.LocaleTestRule;
 import tools.fastlane.screengrab.locale.LocaleUtil;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by mdev on 1/9/2018 AD.
--- app/src/main/java/com/touchtechnologies/fastlanetest/MainActivity.kt
+++ app/src/main/java/com/touchtechnologies/fastlanetest/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.touchtechnologies.fastlanetest
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.touchtechnologies.fastlanetest.utils.StringUtil
 import kotlinx.android.synthetic.main.activity_main.*
